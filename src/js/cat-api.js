bd619422c947dab76d8a66f6ca03cb1cf7dda43a
R3enox <129979133+R3enox@users.noreply.github.com>
2023-09-09 19:35:02 +0300

    update axios

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,27 +1,19 @@
-// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_mlIgmWg4TVZBfi9Gtrj1sdv5250e9IGGoiYh5g8b3DcvRrHcRS7BffaE1dalL4li
+import axios from 'axios';
 
-const BASE_URL = "https://api.thecatapi.com/v1/";
-const END_POINT = "breeds";
-const options = {
-  headers: {
-    "x-api-key":
-      "live_mlIgmWg4TVZBfi9Gtrj1sdv5250e9IGGoiYh5g8b3DcvRrHcRS7BffaE1dalL4li",
-  },
-};
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const END_POINT = 'breeds';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_mlIgmWg4TVZBfi9Gtrj1sdv5250e9IGGoiYh5g8b3DcvRrHcRS7BffaE1dalL4li';
+
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}${END_POINT}`, options).then((resp) => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
+  return axios.get(`${BASE_URL}${END_POINT}`).then(resp => {
+    return resp.data;
   });
 }
 
 export function fetchCatByBreed() {
-  return fetch(`${BASE_URL}${END_POINT}`, options).then((resp) => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
+  return axios.get(`${BASE_URL}${END_POINT}`).then(resp => {
+    return resp.data;
   });
 }
--- src/js/objRefs.js
+++ src/js/objRefs.js
@@ -1,6 +1,6 @@
 export const obj = {
-  select: document.querySelector(".breed-select"),
-  load: document.querySelector(".loader"),
-  loadError: document.querySelector(".error"),
-  catInfo: document.querySelector(".cat-info"),
+  select: document.querySelector('.breed-select'),
+  load: document.querySelector('.loader'),
+  loadError: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
 };
